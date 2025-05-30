64b3e575a63fc1a6251cabd8b20f90ca206f81e3
Reyezando <reynardo.etantyo2@gmail.com>
2025-05-31 00:17:52 +0700

    update javascript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 function clickInput () {
     document.getElementById('fileInput').click()
 }
+
+function printVideos () {
+    const body = document.getElementsByClassName('body')
+    body.innerHTML = '' // Clear previous content
+    const fileInput = document.getElementById('fileInput').files
+    const page = Math.ceil(fileInput.length/6)
+    const k = 0;
+
+    for (let i = 0; i <= page; i++) {
+
+        for (let j = 0; j <= 6; j++) {
+            fileInput[k]
+        }
+    }
+}
